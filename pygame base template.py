3997baa02f706e462da14547395627ec8e762579
shovals <shoval.shalev@gmail.com>
2017-11-08 10:04:42 +0200

    Base template for pygame. Includes color settings, screen color, events section and quit handling

--- pygame base template.py
+++ pygame base template.py
@@ -1,12 +1,14 @@
 import pygame
 pygame.init()
 
+# Set up basic colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
+# init of screen size and title
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Shoval's first game")
